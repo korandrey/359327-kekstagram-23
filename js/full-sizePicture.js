4fad7af3da34a8cc1da963805f8236af3336078d
Андрей Коршунов <andrey@192.168.1.5>
2021-07-15 11:34:13 +0300

    Исправление ошибок с помощью  npm

--- js/full-sizePicture.js
+++ js/full-sizePicture.js
@@ -1,15 +1,15 @@
 import { generatePhotos } from './data.js';
 
 const photo = document.querySelector('.picture__img');
-photo.addEventListener('click', function () {
-    const sectionFullSizePictures = document.querySelector('.big-picture');
-    sectionFullSizePictures.classList.remove('hidden');
-    const showBigPhoto = sectionFullSizePictures.querySelector('.big-picture__img');
-    showBigPhoto.querySelector('img').setAttribute('src', generatePhotos[0].url);
-    sectionFullSizePictures.querySelector('.likes-count').textContent = generatePhotos[0].likes;
-    sectionFullSizePictures.querySelector('.comments-count').textContent = generatePhotos[0].comments[0].id;
-    const socialComments = sectionFullSizePictures.querySelector('.social__comments');
-    socialComments.innerHTML =
+photo.addEventListener('click', () => {
+  const sectionFullSizePictures = document.querySelector('.big-picture');
+  sectionFullSizePictures.classList.remove('hidden');
+  const showBigPhoto = sectionFullSizePictures.querySelector('.big-picture__img');
+  showBigPhoto.querySelector('img').setAttribute('src', generatePhotos[0].url);
+  sectionFullSizePictures.querySelector('.likes-count').textContent = generatePhotos[0].likes;
+  sectionFullSizePictures.querySelector('.comments-count').textContent = generatePhotos[0].comments[0].id;
+  const socialComments = sectionFullSizePictures.querySelector('.social__comments');
+  socialComments.innerHTML =
         `<li class="social__comment">
 <img
     class="social__picture"
@@ -18,20 +18,21 @@ photo.addEventListener('click', function () {
     width="35" height="35">
 <p class="social__text">${generatePhotos[0].comments[0].message}</p>
 </li>`;
-    sectionFullSizePictures.querySelector('.social__caption').textContent = generatePhotos[0].description;
-    sectionFullSizePictures.querySelector('.social__comment-count').classList.add('hidden');
-    sectionFullSizePictures.querySelector('.comments-loader').classList.add('hidden');
-    document.querySelector('body').classList.add('modal-open');
-    const bigPictureCancel = sectionFullSizePictures.querySelector('.big-picture__cancel');
-    bigPictureCancel.addEventListener('click', function () {
-        sectionFullSizePictures.classList.add('hidden');
-        document.querySelector('body').classList.remove('modal-open')
-    });
+  sectionFullSizePictures.querySelector('.social__caption').textContent = generatePhotos[0].description;
+  sectionFullSizePictures.querySelector('.social__comment-count').classList.add('hidden');
+  sectionFullSizePictures.querySelector('.comments-loader').classList.add('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  const bigPictureCancel = sectionFullSizePictures.querySelector('.big-picture__cancel');
+  bigPictureCancel.addEventListener('click', () => {
+    sectionFullSizePictures.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+  });
 
-    document.addEventListener('keydown', function (evt) {
-        if (evt.keyCode === 27) {
-            sectionFullSizePictures.classList.add('hidden');
-            document.querySelector('body').classList.remove('modal-open')
-        }
-    });
+  document.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 27) {
+      sectionFullSizePictures.classList.add('hidden');
+      document.querySelector('body').classList.remove('modal-open');
+    }
+  });
 });
+
